8f9ceec81e6bc8e90b2ba38c5236903a14ce1e0c
ValerioScharizza <valerio.schiarizza@live.it>
2022-09-07 13:45:28 +0200

    Created Array and console log

--- js/script.js
+++ js/script.js
@@ -1,21 +1,83 @@
 console.log('JS OK!');
 
 /*
-Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare
+i membri del team.
 Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
 MILESTONE 0:
 Creare l’array di oggetti con le informazioni fornite.
+
 MILESTONE 1:
 Stampare su console le informazioni di nome, ruolo e la stringa della foto
+
 MILESTONE 2:
 Stampare le stesse informazioni su DOM sottoforma di stringhe
+
 BONUS 1:
 Trasformare la stringa foto in una immagine effettiva
+
 BONUS 2:
 Organizzare i singoli membri in card/schede
+
 Consigli del giorno:
 Ragioniamo come sempre a step.
 Prima la logica in italiano e poi traduciamo in codice.
 */
 
+/*
+Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
+Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
+Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
+Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+Scott Estrada	Developer	scott-estrada-developer.jpg
+Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
+*/
 
+
+//MILESTONE 0:
+//Creare l’array di oggetti con le informazioni fornite.
+
+const team = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        picture: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Carrol',
+        role: 'Chief Editor',
+        picture: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        picture: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        picture: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        picture: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        picture: 'barbara-ramos-graphic-designer.jpg'
+    },
+]
+
+const teamContainer = document.getElementById('team-members-area');
+
+
+// MILESTONE 1:
+// Stampare su console le informazioni di nome, ruolo e la stringa della foto
+
+for (let i = 0; i < team.length; i++) {
+    let teamMember = team[i];
+    console.log(`name: ${teamMember.name} - role: ${teamMember.role} - picture: ${teamMember.picture}`);
+}
